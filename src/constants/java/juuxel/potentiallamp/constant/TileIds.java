eb364b97669850ef5d9642c2f19cd1feadca5f9b
Juuz <6596629+Juuxel@users.noreply.github.com>
2025-09-03 19:37:45 +0300

    Unpick tile ids

---
+++ src/constants/java/juuxel/potentiallamp/constant/TileIds.java
@@ -0,0 +1,57 @@
+package juuxel.potentiallamp.constant;
+
+public final class TileIds {
+    public static final int EMPTY = 0;
+    public static final int ROCK = 1;
+    public static final int GRASS = 2;
+    public static final int DIRT = 3;
+    public static final int STONE_BRICK = 4;
+    public static final int WOOD = 5;
+    public static final int BUSH = 6;
+    public static final int UNBREAKABLE = 7;
+    public static final int WATER = 8;
+    public static final int CALM_WATER = 9;
+    public static final int LAVA = 10;
+    public static final int CALM_LAVA = 11;
+    public static final int SAND = 12;
+    public static final int GRAVEL = 13;
+    public static final int GOLD_ORE = 14;
+    public static final int IRON_ORE = 15;
+    public static final int COAL_ORE = 16;
+    public static final int LOG = 17;
+    public static final int LEAVES = 18;
+    public static final int SPONGE = 19;
+    public static final int GLASS = 20;
+    public static final int RED_CLOTH = 21;
+    public static final int ORANGE_CLOTH = 22;
+    public static final int YELLOW_CLOTH = 23;
+    public static final int CHARTREUSE_CLOTH = 24;
+    public static final int GREEN_CLOTH = 25;
+    public static final int SPRING_GREEN_CLOTH = 26;
+    public static final int CYAN_CLOTH = 27;
+    public static final int CAPRI_CLOTH = 28;
+    public static final int ULTRAMARINE_CLOTH = 29;
+    public static final int VIOLET_CLOTH = 30;
+    public static final int PURPLE_CLOTH = 31;
+    public static final int MAGENTA_CLOTH = 32;
+    public static final int ROSE_CLOTH = 33;
+    public static final int DARK_GRAY_CLOTH = 34;
+    public static final int LIGHT_GRAY_CLOTH = 35;
+    public static final int WHITE_CLOTH = 36;
+    public static final int DANDELION = 37;
+    public static final int ROSE = 38;
+    public static final int BROWN_MUSHROOM = 39;
+    public static final int RED_MUSHROOM = 40;
+    public static final int GOLD_BLOCK = 41;
+    public static final int IRON_BLOCK = 42;
+    public static final int DOUBLE_SLAB = 43;
+    public static final int SLAB = 44;
+    public static final int BRICKS = 45;
+    public static final int TNT = 46;
+    public static final int BOOKSHELF = 47;
+    public static final int MOSSY_COBBLESTONE = 48;
+    public static final int OBSIDIAN = 49;
+
+    private TileIds() {
+    }
+}
