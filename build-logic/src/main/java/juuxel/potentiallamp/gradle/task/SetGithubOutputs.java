2a1a13314e7fc9f2d366627fb20befcc3c40a574
Juuz <6596629+Juuxel@users.noreply.github.com>
2025-09-03 15:49:58 +0300

    SetGithubOutputs: Use output file instead of stdout

--- build-logic/src/main/java/juuxel/potentiallamp/gradle/task/SetGithubOutputs.java
+++ build-logic/src/main/java/juuxel/potentiallamp/gradle/task/SetGithubOutputs.java
@@ -1,19 +1,40 @@
 package juuxel.potentiallamp.gradle.task;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.provider.MapProperty;
 import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.util.Map;
 
 public abstract class SetGithubOutputs extends DefaultTask {
     @Input
     public abstract MapProperty<String, String> getOutputProperties();
 
+    @OutputFile
+    public abstract RegularFileProperty getOutputFile();
+
+    public SetGithubOutputs() {
+        String outputFile = System.getenv("GITHUB_OUTPUT");
+        if (outputFile != null) {
+            getOutputFile().convention(getProject().getLayout().file(getProject().provider(() -> new File(outputFile))));
+        }
+    }
+
     @TaskAction
-    protected void setOutputs() {
-        // TODO: switch to the non-deprecated way of doing this
-        getOutputProperties().get().forEach((key ,value) -> {
-            System.out.printf("::set-output name=%s::%s%n", key, value);
-        });
+    protected void setOutputs() throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(getOutputFile().get().getAsFile().toPath());
+             PrintWriter pw = new PrintWriter(writer)) {
+            for (Map.Entry<String, String> entry : getOutputProperties().get().entrySet()) {
+                pw.printf("%s=%s%n", entry.getKey(), entry.getValue());
+            }
+        }
     }
 }
